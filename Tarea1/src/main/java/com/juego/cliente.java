c71a9f6e882221edc5dceb230463a601c029c2d3
Katzx3 <48260067+Katzx3@users.noreply.github.com>
2022-09-16 10:54:35 -0600

    Actualizar link de UML
    Actualizar link de uml

--- Tarea1/src/main/java/com/juego/cliente.java
+++ Tarea1/src/main/java/com/juego/cliente.java
@@ -6,33 +6,32 @@ import java.io.IOException;
 public class cliente {
 
     public static void main(String[] args) throws IOException {
-        mundo nivel2 = new mundo(20,50,"*");
-        nivel2.dibujo();
-        System.out.println(" ");
+        //mundo nivel2 = new mundo(20,50,"*");
+        //nivel2.dibujo();
+        //System.out.println(" ");
 
         mapa nuevoNivel = new mapa();
         nuevoNivel.plantillaMapa();
 
-        System.out.println(" ");
+        //System.out.println(" ");
 
-        personaje heroe1 = new Heroe();
-        heroe1.accionArma();
-        heroe1.accionItems();
-        heroe1.accionMagia();
-        heroe1.display();
+        //personaje heroe1 = new Heroe();
+        //heroe1.accionArma();
+        //heroe1.accionItems();
+        //heroe1.accionMagia();
+       // heroe1.display();
 
-        System.out.println(" ");
+        //System.out.println(" ");
 
-        personaje Magus = new Villano();
-        Magus.display();
-        Magus.accionMagia();
-        Magus.accionPelear();
-        Magus.accionArma();
+       // personaje Magus = new Villano();
+        ////Magus.accionMagia();
+        //Magus.accionPelear();
+        //Magus.accionArma();
 
-        CloneFactory nuevoClon = new CloneFactory();
-        Bat dracula = new Bat();
-        Bat clonedBat = (Bat) nuevoClon.copiar(dracula);
-        System.out.println(dracula);
+        //CloneFactory nuevoClon = new CloneFactory();
+        //Bat dracula = new Bat();
+        //Bat clonedBat = (Bat) nuevoClon.copiar(dracula);
+        //System.out.println(dracula);
 
 
 
